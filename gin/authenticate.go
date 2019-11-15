cd3bc06f0a34139931197b58d2dbac6b0bb8cedf
farrukh <fprotimaru@gmail.com>
2019-11-15 11:22:43 +0500

    last

--- gin/authenticate.go
+++ gin/authenticate.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+type UserModel struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type User interface {
-	getByUsername(string) interface{}
+	getByUsername(string) bool
 	getUsername() string
 	getPassword() string
+	checkPassword(string) bool
 }
 
 type Authenticate struct {
@@ -18,7 +30,44 @@ type Authenticate struct {
 }
 
 func (a Authenticate) LoginController(c *gin.Context) {
-
+	var err error
+	var user UserModel
+	err = c.BindJSON(&user)
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{
+			"error": "username or password is empty",
+		})
+		return
+	}
+	if user.Username == "" || user.Password == "" {
+		c.AbortWithStatusJSON(400, gin.H{
+			"error": "username or password is empty",
+		})
+		return
+	}
+	if ok := a.User.getByUsername(user.Username); !ok {
+		c.AbortWithStatusJSON(404, gin.H{
+			"error": "user not found",
+		})
+		return
+	}
+	if ok := a.User.checkPassword(user.Password); !ok {
+		c.AbortWithStatusJSON(404, gin.H{
+			"error": "username or password is incorrect",
+		})
+		return
+	}
+	claims := Claims{
+		Username: user.Username, StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 30).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := token.SignedString([]byte(a.SecretKey))
+	c.JSON(200, gin.H{
+		"token": tokenString,
+	})
+	return
 }
 
 func (a *Authenticate) Middleware() gin.HandlerFunc {
